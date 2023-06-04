523aa2a4c9549c416ed4fb1f766ea7ad397ad512
McKayMower <mmower777@gmail.com>
2023-06-03 23:24:59 -0600

    sidebaritems nice icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 interface SidebarProps {
   children: React.ReactNode;
 }
@@ -30,7 +31,14 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
   return (
     <div className="flex h-full">
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-        <Box>Sidebar Navigation</Box>
+        <Box><div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map(item => (
+                <SidebarItem key={item.label} {...item} >
+
+                </SidebarItem>
+            ))}
+            </div></Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
       </div>
     </div>
   );
