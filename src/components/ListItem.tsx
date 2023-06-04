7a24dfca09ff065b000a4c6391e147a9068a7f43
McKayMower <mmower777@gmail.com>
2023-06-04 09:06:28 -0600

    group play button on liked songs listitem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { FaPlay } from "react-icons/fa";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FC } from "react";
@@ -18,13 +19,14 @@ const ListItem: FC<ListItemProps> = ({ image, name, href }) => {
   };
 
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
       <div className="relative min-h-[64px] min-w-[64px]">
         <Image src={image} className="object-cover" fill alt="Image" />
       </div>
-      <p>
-        {name}
-      </p>
+      <p className="font-medium truncate py-5">{name}</p>
+      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+        <FaPlay className="text-black" />
+      </div>
     </button>
   );
 };
