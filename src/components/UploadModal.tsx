82c9dc67ac90d3f4c2194c4d21e679dc7f204afe
McKayMower <mmower777@gmail.com>
2023-06-05 23:30:38 -0600

    upload image/song

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -4,11 +4,19 @@ import Modal from "./Modal";
 import useUploadModal from "@/hooks/useUploadModal";
 import { SubmitHandler, FieldValues, useForm } from "react-hook-form";
 import Input from "./Input";
+import Button from "./Button";
+import { toast } from "react-hot-toast";
+import { useUser } from "@/hooks/useUser";
+import uniqid from "uniqid";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
 interface UploadModalProps {}
 
 const UploadModal: FC<UploadModalProps> = ({}) => {
   const uploadModal = useUploadModal();
   const [isLoading, setIsLoading] = useState(false);
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
@@ -26,7 +34,54 @@ const UploadModal: FC<UploadModalProps> = ({}) => {
     }
   };
 
-  const onSubmit: SubmitHandler<FieldValues> = async (values) => {};
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    try {
+      setIsLoading(true);
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+
+      if (!imageFile || !songFile || !user) {
+        toast.error("Missing Fields");
+        return;
+      }
+
+      const uniqueID = uniqid();
+
+      // upload song
+
+      const { data: songData, error: songError } = await supabaseClient.storage
+        .from("songs")
+        .upload(`song-${values.title}-${uniqueID}`, songFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+        if(songError) {
+            setIsLoading(false)
+            return toast.error("Failed song upload")
+        }
+
+        // upload image
+
+        const { data: imageData, error: imageError } = await supabaseClient.storage
+        .from("images")
+        .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+        if(imageError) {
+            setIsLoading(false)
+            return toast.error("Failed image upload")
+        }
+
+        
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Modal
@@ -35,13 +90,42 @@ const UploadModal: FC<UploadModalProps> = ({}) => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
         <Input
           id="title"
           disabled={isLoading}
           placeholder="Song Title"
           {...register("title", { required: true })}
         />
+        <Input
+          id="author"
+          disabled={isLoading}
+          placeholder="Song Author"
+          {...register("author", { required: true })}
+        />
+        <div>
+          <div className="pb-1">Select a song file</div>
+          <Input
+            type="file"
+            id="song"
+            disabled={isLoading}
+            accept=".mp3"
+            {...register("song", { required: true })}
+          />
+        </div>
+        <div>
+          <div className="pb-1">Select an image</div>
+          <Input
+            type="file"
+            id="image"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+          />
+        </div>
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
       </form>
     </Modal>
   );
