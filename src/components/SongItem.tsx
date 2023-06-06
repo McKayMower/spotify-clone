5b5ecdb7dacb874e055391720408fae32db67ebd
McKayMower <mmower777@gmail.com>
2023-06-05 23:58:58 -0600

    play button

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -3,6 +3,7 @@ import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
 import { FC } from "react";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   onClick: (id: string) => void;
@@ -25,11 +26,14 @@ const SongItem: FC<SongItemProps> = ({ onClick, data }) => {
           fill
         />
       </div>
-      <div className="flex flex-col items-start w-full p-4 gap-y-1">
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
         <p className="font-semibold truncate w-full">{data.title}</p>
         <p className="text-neutral-400 text-sm pb-4 w-full truncate">
           By {data.author}
         </p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
       </div>
     </div>
   );
