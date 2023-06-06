91568dd9d2277d7795d0170807a2f004ba2c9e93
McKayMower <mmower777@gmail.com>
2023-06-06 00:12:30 -0600

    library media item

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -6,6 +6,7 @@ import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
 interface LibraryProps {
   songs: Song[];
@@ -41,7 +42,7 @@ const Library: FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <MediaItem key={item.id} />
+          <MediaItem key={item.id} onClick={() => {}} data={item} />
         ))}
       </div>
     </div>
