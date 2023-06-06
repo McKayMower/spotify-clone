787da1d3f80dcdd0876fd482cc5c552202fea89d
McKayMower <mmower777@gmail.com>
2023-06-05 23:56:05 -0600

    song card started

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -13,9 +13,9 @@ const PageContent: FC<PageContentProps> = ({ songs }) => {
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
-        {songs.map((item) => (
-            <SongItem key={item.id} onClick={() => {}} data={item}/>
-        ))}
+      {songs.map((item) => (
+        <SongItem key={item.id} onClick={() => {}} data={item} />
+      ))}
     </div>
   );
 };
