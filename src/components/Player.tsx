501389b0fefc38630be89c84b9c1b02b04eaa760
McKayMower <mmower777@gmail.com>
2023-06-07 01:01:15 -0600

    onPlay hook

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/userPlayer";
 import { FC } from "react";
 
@@ -10,9 +11,17 @@ const Player: FC<PlayerProps> = ({}) => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
 
-  const songUrl = useLoadSongUrl(song)
+  const songUrl = useLoadSongUrl(song!);
 
-  return <div>Player</div>;
+  if (!song || !songUrl || !player.activeId) return null;
+
+  
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      Player
+    </div>
+  );
 };
 
 export default Player;
