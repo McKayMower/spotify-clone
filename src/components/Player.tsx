9a23a2d0b2b34c9c8d84bae4513d863a5bc643a2
McKayMower <mmower777@gmail.com>
2023-06-08 14:37:02 -0600

    Slider, Player , PlayerContent

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,6 +4,7 @@ import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/userPlayer";
 import { FC } from "react";
+import PlayerContent from "./PlayerContent";
 
 interface PlayerProps {}
 
@@ -15,11 +16,9 @@ const Player: FC<PlayerProps> = ({}) => {
 
   if (!song || !songUrl || !player.activeId) return null;
 
-  
-
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent song={song} songUrl={songUrl} key={songUrl} />
     </div>
   );
 };
